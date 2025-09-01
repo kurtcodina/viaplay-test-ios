67fab4ab5336b526ba541971ada0cab575348eb8
kurtcodina <128434639+kurtcodina@users.noreply.github.com>
2025-09-01 19:11:47 +0800

    Initial Commit

---
+++ Viaplay-Test/Viaplay_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viaplay_TestApp.swift
+//  Viaplay-Test
+//
+//  Created by codina on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Viaplay_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
